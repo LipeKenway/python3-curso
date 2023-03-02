f445481475f840b1bd0303a8b4c4a2817ce33490
LipeKenway <lyppys43@gmail.com>
2023-03-01 21:56:58 -0300

    Curso Python 3

--- intermediario/list_comprehension/dict_comprehension_e_set_comprehension.py
+++ intermediario/list_comprehension/dict_comprehension_e_set_comprehension.py
@@ -6,3 +6,25 @@ produto = {
     'preco': 2.5,
     'categoria': 'Escritorio',
 }
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor # <- Mapeamento
+    for chave, valor 
+    in produto.items()
+    if chave == 'categoria' # <- Filtro
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor a'),
+    ('b', 'valor a'),
+]
+
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
