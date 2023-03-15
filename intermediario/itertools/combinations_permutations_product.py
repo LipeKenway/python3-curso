6c5f2aa25a6ae9f38da944170cfe488ff00ed733
LipeKenway <lyppys43@gmail.com>
2023-03-14 22:15:13 -0300

    Curso Python 3

---
+++ intermediario/itertools/combinations_permutations_product.py
@@ -0,0 +1,12 @@
+"""
+# Combinations, Permutations e Product -> Itertools
+
+-> Combinação -> Ordem não importa -> iteravel + tamanho do grupo.
+-> Permutação -> Ordem importa.
+-> Produto -> Ordem importa e repete valores unicos.
+"""
+pessoas = ['Pedro', 'Nadia', 'Maria', 'Felipe']
+
+camisetas =[ 
+    ['preta', 'branca'],
+]
