957366b9d12c6f2799355d273284c68bd948ee75
LipeKenway <lyppys43@gmail.com>
2023-03-20 20:13:58 -0300

    Curso Python 3

---
+++ intermediario/ambiente_virtual/venv.py
@@ -0,0 +1,29 @@
+"""
+# Ambientes virtuais em Python (venv)
+
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+
+# Ao ativar um ambiente virtual, a instalação do ambiente
+# virtual será usada.
+
+# venv eh o modulo que vamos usar para criar ambientes virtuais.
+
+# Vc pode dar o nome que preferir para um ambiente virtual, mas
+# os mais comuns são (venv, env, .venv e .env).
+
+# Como criar um ambiente virtual
+# Abra a pasta do seu projeto no terminal
+# e digite: python -m venv venv
+
+# Alguns comandos:
+
+# python -m pip install pymysql
+# pip install pip --upgrade
+# pip install pymysql
+# pip install -r .\requirements.txt
+# pip uninstall pymysql -y
+# pip freeze
+# pip freeze > requirements.txt
+# pip index versions pymysql
+"""
