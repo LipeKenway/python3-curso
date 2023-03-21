83937e5468219b86ba4d4c7e1f3a8c70ae37dde5
LipeKenway <lyppys43@gmail.com>
2023-03-20 22:19:26 -0300

    Curso Python 3

---
+++ intermediario/manipulacao-de-arquivos/criando_arquivos_com_python.py
@@ -0,0 +1,33 @@
+"""
+# Criando arquivos com Python 3
+
+# Usamos a função open para abrir 
+# um arquivo Python (ele pode ou não existir).
+
+# Modos: 
+    ! r -> (leitura), 
+    ! w -> (escrita),
+    ! x -> (para criação),
+    ! a -> (escreve ao final),
+    ! b -> (binario),
+    ! t -> (modo texto),
+    ! + -> (leitura e escrita).
+
+# Context Manager -> with (abre e fecha).
+
+# Metodos Uteis:
+    ! write, read -> (escrever e ler),
+    ! writelines -> (escrever varias linhas),
+    ! seek (mover o cursor),
+    ! readline -> (ler linha),
+    ! readlines -> (ler linhas).
+
+# Vamos falar mais sobre o modulo os, mas:
+    ! os.remove ou unline -> (apaga o arquivo),
+    ! os.rename -> (troca o nome ou move o arquivo).
+
+# Vamos falar mais sobre o modulo JSON, mas:
+    ! json.dump -> (gera um arquivo .json),
+    ! json.load ->
+
+"""
