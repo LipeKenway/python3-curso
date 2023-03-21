d7a3d0a1a9cd636fd477cf1b7b015250eeee0f4e
LipeKenway <lyppys43@gmail.com>
2023-03-21 20:52:09 -0300

    Curso Python 3

--- intermediario/manipulacao-de-arquivos/criando_arquivos_com_python.py
+++ intermediario/manipulacao-de-arquivos/criando_arquivos_com_python.py
@@ -1,11 +1,11 @@
 """
 # Criando arquivos com Python 3
 
-# Usamos a função open para abrir 
+# Usamos a função open para abrir
 # um arquivo Python (ele pode ou não existir).
 
-# Modos: 
-    ! r -> (leitura), 
+# Modos:
+    ! r -> (leitura),
     ! w -> (escrita),
     ! x -> (para criação),
     ! a -> (escreve ao final),
@@ -30,12 +30,40 @@
     ! json.dump -> (gera um arquivo .json),
     ! json.load ->
 """
-caminho_arquivo = 'C:\\Users\\lyppy\\Downloads\\Cursos\\python\\intermediario\\manipulacao-de-arquivos\\'
+import os
+
+caminho_arquivo = r'C:\\Users\\lyppy\\Downloads\\Cursos\\python\\intermediario\\manipulacao-de-arquivos\\'
+os.chdir(caminho_arquivo)
 caminho_arquivo += 'aula_teste.txt'
+
 
 # arquivo = open(caminho_arquivo, 'w')
 # arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá Mundo')
-    print('O arquivo vai ser fechado.')
+with open(caminho_arquivo, 'w+', encoding='utf-8') as arquivo:
+    print('ESCREVENDO')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(('Linha 3 \n', 'Linha 4\n'))
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+
+    print('=' * 20)
+
+    print('LENDO')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('=' * 20)
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+print('=' * 20)
+
+# os.rename(caminho_arquivo, 'aula_teste.txt')
+# os.unlink(caminho_arquivo)
---
+++ intermediario/salvando-dados-python-com-json/dados_python_em_json.py
@@ -0,0 +1,15 @@
+"""
+# Salvando dados Python em JSON
+"""
+pessoa = {
+    'nome': 'Felipe',
+    'sobrenome': 'Soares',
+    'endereco': [
+        {'rua': 'R1', 'numero': 111},
+        {'rua': 'R2', 'numero': 256}
+    ],
+    'altura': 1.8,
+    'numero_preferido': (2, 7, 11),
+    'dev': True,
+    'nada': None
+}
